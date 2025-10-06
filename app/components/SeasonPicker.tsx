6927d73e707e3fede675284c1bc572395956588e
jradoch <janrdch@gmail.com>
2025-10-06 14:30:44 +0200

    fix dark theme for seasonPicker

--- app/components/SeasonPicker.tsx
+++ app/components/SeasonPicker.tsx
@@ -9,8 +9,8 @@ export default function SeasonPicker() {
     };
 
     return (
-        <div className="p-4 flex justify-center items-center dark:bg-gray-800">
-            <select value={seasonId ?? ""} onChange={handleChange} className="block w-50 rounded-lg border border-gray-300 bg-white py-2 px-3 text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+        <div className="p-4 flex justify-center items-center">
+            <select value={seasonId ?? ""} onChange={handleChange} className="block w-50 rounded-lg border border-gray-300 py-2 px-3 dark:bg-black dark:text-white text-gray-700 shadow-sm focus:border-blue-500 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
                 <option value="" disabled>Select season</option>
                 <option value="1">Umelka 2025</option>
                 <option value="2">Salovka 2025/2026</option>
