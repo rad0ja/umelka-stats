bde5f6840c97defc231697f07d42d1d2b7489a5f
jradoch <janrdch@gmail.com>
2025-12-02 10:55:24 +0100

    comment SignUp

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,13 +1,11 @@
-// 'use client'
-//
-// import SeasonPicker from "@/app/components/SeasonPicker";
-// import SignUp from "@/app/components/SignUp";
-//
-// export default function Settings() {
-//     return (
-//         <div className="max-w-4xl mx-auto p-6">
-//             <h1 className="text-2xl font-semibold mb-4">Settings</h1>
-//             <SignUp />
-//         </div>
-//     )
-// }
+'use client'
+
+import SeasonPicker from "@/app/components/SeasonPicker";
+
+export default function Settings() {
+   return (
+        <div className="max-w-4xl mx-auto p-6">
+            <h1 className="text-2xl font-semibold mb-4">Settings</h1>
+         </div>
+     )
+ }
